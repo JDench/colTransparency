32c527b5e3020e6f38a906277895b00805d5a1f7
JDench <34353759+JDench@users.noreply.github.com>
2018-04-18 12:13:05 -0400

    Vectors of parms now work properly

--- colTransparency.v.1.r
+++ colTransparency.v.1.r
@@ -2,34 +2,35 @@
 # a range of basic named colours to be dynamically manipulted into altered vectors of the colour. 
 colTransparency <- function(func_cols, func_opacity=1, func_scaleSaturation = NA, func_scaleValue = NA){
 	# In case the user has passed scaling values outside of the 0-1 range we simply return them to being values of 1
-	funcParms = c("Saturation"= func_scaleSaturation,
+	funcParms = list("Saturation"= func_scaleSaturation,
 					"Value"= func_scaleValue,
 					"Opacity"= func_opacity)
 	# We check that the opacity value is within the range of 0-1				
 	for(thisParm in names(funcParms)){
 		# If the user has not defined anything, then let it pass....
-		if(!is.na(funcParms[thisParm])){
-			if(funcParms[thisParm] > 1){
-				funcParms[thisParm] <- 1
-			} else if (funcParms[thisParm] < 0){
-				funcParms[thisParm] <- 0
+		if(all(!is.na(funcParms[[thisParm]]))){
+			if(any(funcParms[[thisParm]] > 1)){
+				funcParms[[thisParm]][which(funcParms[thisParm] > 1)] <- 1
+			} else if (any(funcParms[[thisParm]] < 0)){
+				funcParms[[thisParm]][which(funcParms[thisParm] < 0)] <- 0
 			}
 		}
 	}
 	
 	# After extracting the RGB balues of a colour we change the values based on the opacity.
-  	return( apply(rgb2hsv(sapply(func_cols, col2rgb)), MARGIN = 2,function(x){
-  				hsv(h = x[1], 
-  					s = if(is.na(funcParms["Saturation"])){
-  							x[2]
-  						} else {
-  							funcParms["Saturation"]
-  						}, 
-  					v = if(is.na(funcParms["Value"])){
-  							x[3]
-  						} else {
-  							funcParms["Value"]
-  						}, 
-  					alpha= funcParms["Opacity"]) 
-  			}) )
+  	tmpReturn <- rgb2hsv(sapply(func_cols, col2rgb))
+  	return( sapply(1:ncol(tmpReturn),function(x){
+	  				hsv(h = tmpReturn[1,x], 
+	  					s = if(any(is.na(funcParms[["Saturation"]]))){
+	  							tmpReturn[2,x]
+	  						} else {
+	  							funcParms[["Saturation"]]
+	  						}, 
+	  					v = if(any(is.na(funcParms[["Value"]]))){
+	  							tmpReturn[3,x]
+	  						} else {
+	  							funcParms[["Value"]]
+	  						}, 
+	  					alpha= funcParms[["Opacity"]]) 
+	  			}) )
 }
